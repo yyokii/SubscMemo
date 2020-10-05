6e46bd7c292497a94a3673647bf215b2d15021e6
yoki <yyokii.h@gmail.com>
2020-10-06 00:45:51 +0900

    Initial Commit

---
+++ SubscMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubscMemo
+//
+//  Created by 東原与生 on 2020/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SubscMemo/SubscMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscMemoApp.swift
+//  SubscMemo
+//
+//  Created by 東原与生 on 2020/10/06.
+//
+
+import SwiftUI
+
+@main
+struct SubscMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8e17476775e70dea7bb2bc27c54858edb6dc26ee
yyokii <yyokii.h@gmail.com>
2021-06-13 21:26:16 +0900

    ✨ Add Extention BindingDefaultValue

---
+++ SubscMemo/Extention/BindingDefaultValue.swift
@@ -0,0 +1,15 @@
+//
+//  BindingDefaultValue.swift
+//  SubscMemo
+//
+//  Created by Higashihara Yoki on 2021/06/12.
+//
+
+import SwiftUI
+
+func ??<T>(lhs: Binding<T?>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}

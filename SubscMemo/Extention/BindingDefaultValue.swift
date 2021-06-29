5f791d19ac6f5af6d54e479487ff719e174aa237
yyokii <yyokii.h@gmail.com>
2021-06-29 23:15:46 +0900

    🩹 Fix Warnig

--- SubscMemo/Extention/BindingDefaultValue.swift
+++ SubscMemo/Extention/BindingDefaultValue.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-func ??<T>(lhs: Binding<T?>, rhs: T) -> Binding<T> {
+func ?? <T>(lhs: Binding<T?>, rhs: T) -> Binding<T> {
     Binding(
         get: { lhs.wrappedValue ?? rhs },
         set: { lhs.wrappedValue = $0 }

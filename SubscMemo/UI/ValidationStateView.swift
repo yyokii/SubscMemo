9ae9286f8e3b7155e5519353bf46b96f676bac5b
yyokii <yyokii.h@gmail.com>
2021-07-13 03:38:48 +0900

    🩹 Fix ValidationStateView

--- SubscMemo/UI/ValidationStateView.swift
+++ SubscMemo/UI/ValidationStateView.swift
@@ -8,21 +8,55 @@
 import SwiftUI
 
 struct ValidationStateView: View {
-
-    @ObservedObject var vm = ValidationStateViewModel()
+    @ObservedObject var vm: ValidationStateViewModel
 
     var body: some View {
-        vm.state.flatMap { state -> AnyView in
-
-            switch state {
-            case .invalid:
-                return AnyView(Image(systemName: "xmark.circle")
-                                .foregroundColor(Color.red))
-            case .valid:
-                return AnyView(Image(systemName: "checkmark.circle")
-                                .foregroundColor(Color.green))
-
+        vm.state.flatMap {
+            view(with: $0)
+        }
+        .adaptiveFont(.matterMedium, size: 12)
+    }
+    
+    func view(with state: ValidationStateViewModel.ValidationResult) -> some View {
+        switch state {
+        case .invalid(let message):
+            return HStack {
+                Image(systemName: "xmark.circle")
+                    .foregroundColor(Color.red)
+                Text(message)
+            }
+        case .valid(let message):
+            return HStack {
+                Image(systemName: "checkmark.circle")
+                    .foregroundColor(Color.green)
+                Text(message)
             }
         }
     }
 }
+
+#if DEBUG
+
+struct ValidationStateView_Previews: PreviewProvider {
+    
+    static let validVM: ValidationStateViewModel = {
+        let vm = ValidationStateViewModel()
+        vm.state = .valid("OK!")
+        return vm
+    }()
+    
+    static let invalidVM: ValidationStateViewModel = {
+        let vm = ValidationStateViewModel()
+        vm.state = .invalid("No...")
+        return vm
+    }()
+    
+    static var previews: some View {
+        VStack {
+            ValidationStateView(vm: invalidVM)
+            ValidationStateView(vm: validVM)
+        }
+    }
+}
+
+#endif

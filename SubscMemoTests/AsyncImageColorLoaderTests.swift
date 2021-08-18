80b3cc172b3f7224cdd17b6d676bd2c8e7f83d56
yyokii <yyokii.h@gmail.com>
2021-08-18 13:27:50 +0900

    ✅ Add AsyncImageColorLoaderTests

---
+++ SubscMemoTests/AsyncImageColorLoaderTests.swift
@@ -0,0 +1,30 @@
+//
+//  AsyncImageColorLoaderTests.swift
+//  SubscMemoTests
+//
+//  Created by Higashihara Yoki on 2021/08/11.
+//
+
+import SwiftUI
+import XCTest
+@testable import SubscMemo
+
+class AsyncImageColorLoaderTests: XCTestCase {
+
+    func testLoadL_キャッシュされているものはキャッシュより取得される() {
+        // given
+        let url = URL(string: "demo")!
+        let expectedColor: Color = .gray
+
+        let loader = AsyncImageColorLoader(url: url)
+        let stubCache = CacheImpl<URL, Color>()
+        stubCache[url] = expectedColor
+        AsyncImageColorLoader.cache = AnyCache(data: stubCache)
+
+        // when
+        loader.load()
+
+        // then
+        XCTAssertEqual(loader.averageColor, expectedColor)
+    }
+}

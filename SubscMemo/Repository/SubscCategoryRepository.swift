02ecbf92a8c02bfd0687cedd6945c190c05aff13
yyokii <yyokii.h@gmail.com>
2021-08-24 15:01:31 +0900

    🩹 Fix fetch category

--- SubscMemo/Repository/SubscCategoryRepository.swift
+++ SubscMemo/Repository/SubscCategoryRepository.swift
@@ -15,50 +15,29 @@ class BaseSubscCategoryRepository {
 
 /// サブスクリプションサービスのカテゴリー一覧データへアクセス
 protocol SubscCategoryRepository: BaseSubscCategoryRepository {
-    func loadCategory() -> AnyPublisher<[SubscCategory], Error>
+    func loadCategory()
 }
 
 final class FirestoreSubscCategoryRepository: BaseSubscCategoryRepository, SubscCategoryRepository, ObservableObject {
 
-    var db: Firestore = Firestore.firestore()
+    let db: Firestore = Firestore.firestore()
 
     private var cancellables = Set<AnyCancellable>()
 
     override init() {
         super.init()
 
-        _ = loadCategory()
+        loadCategory()
     }
 
-    func loadCategory() -> AnyPublisher<[SubscCategory], Error> {
-        return Future<[SubscCategory], Error> { [weak self] promise in
-
-            guard let self = self else {
-                promise(.failure(RepositoryError.other))
-                return
-            }
-
-            self.db
-                .collection(FirestorePathComponent.subscriptionServices.rawValue)
-                .document(FirestorePathComponent.version.rawValue)
-                .collection(FirestorePathComponent.categories.rawValue)
-                .addSnapshotListener { (querySnapshot, error) in
-
-                    if let error = error {
-                        promise(.failure(error))
-                    }
-
-                    if let querySnapshot = querySnapshot {
-                        self.categories = querySnapshot.documents
-                            .compactMap { document -> SubscCategory? in
-                                try? document.data(as: SubscCategory.self)
-                            }
-                        promise(.success(self.categories))
-                    } else {
-                        promise(.failure(RepositoryError.notFound))
-                    }
-                }
-        }
-        .eraseToAnyPublisher()
+    func loadCategory() {
+        db
+            .collection(FirestorePathComponent.subscriptionServices.rawValue)
+            .document(FirestorePathComponent.version.rawValue)
+            .collection(FirestorePathComponent.categories.rawValue)
+            .getDocuments(as: SubscCategory.self)
+            .replaceError(with: categories)
+            .assign(to: \.categories, on: self)
+            .store(in: &cancellables)
     }
 }
